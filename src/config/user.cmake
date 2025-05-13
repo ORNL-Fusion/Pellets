309f103d541d8014c64ede9c948781908c93b349
rhealbarnett <rhealbarnett@gmail.com>
2025-05-13 16:16:33 -0400

    Added config directory with default.cmake (template) and user.cmake to set easily set cmake options.

---
+++ src/config/user.cmake
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------#
+# R.L.Barnett macOS Sequoia 15.4.1 local CMake build for the 
+# PELLET code.
+#------------------------------------------------------------------#
+
+# Set build type
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type: Debug, Release, etc...")
+
+# Check build type and set flags accordingly 
+if(CMAKE_BUILD_TYPE MATCHES "[Dd]ebug")
+  set(FORTRAN_FLAGS "-Og -Wall -Wextra -fcheck=all" CACHE STRING "Fortran compiler flags")
+else()
+  set(FORTRAN_FLAGS "-O2" CACHE STRING "Fortran compiler flags")
+endif()
+
+# Set path for the NETCDF headers and libs.
+set(NETCDF_INC_PATH "/usr/local/include" CACHE PATH "Path to NetCDF include directory")
+set(NETCDF_LIB_PATH "/usr/local/lib" CACHE PATH "Path to NetCDF library directory")
