3e0cd46ee1d555e27584cab8eb07769c18341d4f
rhealbarnett <barnettrl@ornl.gov>
2025-06-25 08:52:09 -0400

    Updated .gitignore

---
+++ src/build/CMakeFiles/xpellet.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xpellet.dir/ajax_mod.f90.o"
+  "CMakeFiles/xpellet.dir/drifts_mod.f90.o"
+  "CMakeFiles/xpellet.dir/fluxav_mod.f90.o"
+  "CMakeFiles/xpellet.dir/linear1_mod.f90.o"
+  "CMakeFiles/xpellet.dir/mirth_mod.f90.o"
+  "CMakeFiles/xpellet.dir/pellet_dr.f90.o"
+  "CMakeFiles/xpellet.dir/pellet_mod.f90.o"
+  "CMakeFiles/xpellet.dir/prl_mod_intel.f90.o"
+  "CMakeFiles/xpellet.dir/setup_ajax.f90.o"
+  "CMakeFiles/xpellet.dir/spec_kind_mod.f90.o"
+  "CMakeFiles/xpellet.dir/spline1_mod.f90.o"
+  "CMakeFiles/xpellet.dir/track_mod.f90.o"
+  "CMakeFiles/xpellet.dir/write_mod.f90.o"
+  "CMakeFiles/xpellet.dir/x_mod.f90.o"
+  "xpellet"
+  "xpellet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/xpellet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
