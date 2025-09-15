49e14dd70da409f9f01ca48c8025cff86d56a636
rhealbarnett <barnettrl@ornl.gov>
2025-09-15 16:30:34 -0400

    Script to run pellet scans.

--- Python/call_pellet_scans.py
+++ Python/call_pellet_scans.py
@@ -12,11 +12,17 @@ r_pel_input = [1.0,1.5,2.0]
 r_pel_input_scaled = np.divide(r_pel_input,pel_old)
 mass_scale = r_pel_input_scaled**3
 d_input = np.multiply(r_pel_input,2.0)
-v_input = [1000,1500]
+# v_input = [1000,1250,1500]
+v_input = [200,300]
 
-print(d_input)
+den0 = [0.40,0.60,0.80,1.0,1.2,1.4]
+den0_scaled = np.multiply(den0,1.0e19)
+den1 = np.multiply(den0_scaled,0.25)
 
 pel.scan_pellet_mass_velocity(base_dir, device = None, runid_prefix = None,
                               diameters_mm = d_input,
                               velocities = v_input,
                               pellet_executable_loc = base_dir + '/build/xpellet')
+
+# pel.scan_density_profile(base_dir,den0 = den0_scaled,
+#                          den1=den1,pellet_executable_loc=xfile)
